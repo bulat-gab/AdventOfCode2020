e5bdb78ef6fde3837d6a0ff62aae3218d50de8f6
bulat-gab <gabdrakhmanov.br@gmail.com>
2020-12-08 22:22:28 +0100

    Add day 6

---
+++ src/AdventOfCode/Day6.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day6
+    {
+        public int Part1()
+        {
+            var input = File.ReadAllText("./input6");
+
+            var groups = input.Split("\r\n\r\n");
+
+            var sum = 0;
+            foreach (var group in groups)
+            {
+                var set = new HashSet<char>();
+
+                var people = group.Split("\r\n");
+                foreach (var p in people)
+                {
+                    foreach (var ch in p.ToCharArray())
+                    {
+                        set.Add(ch);
+                    }
+                }
+                sum += set.Count;
+            }
+            return sum;
+        }
+
+        public int Part2()
+        {
+            var input = File.ReadAllText("./input6");
+            var groups = input.Split("\r\n\r\n");
+            var sum = 0;
+            
+            foreach (var group in groups)
+            {
+                var people = group.Split("\r\n");
+                if (people.Length == 1)
+                {
+                    sum += people[0].Length;
+                    continue;
+                }
+
+                var answersOfFirstPerson = people[0].ToCharArray();
+                foreach (var ch in answersOfFirstPerson)
+                {
+                    if (people.Skip(1).All(x => x.Contains(ch)))
+                    {
+                        sum++;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -6,22 +6,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            // var d1 = new Day1().Part2();
-            // Console.WriteLine(d1);
-            
-            // var d2 = new Day2.Day2().Part2();
-            // Console.WriteLine(d2);
-            
-            // var d3 = new Day3().Part2();
-            // Console.WriteLine(d3);
-
-            // var d4 = new Day4().Part2();
-            // Console.WriteLine(d4);
-
-            // var d5 = new Day5().Part1();
-            // Console.WriteLine(d5);
-            
-            var d = new Day8().Part2();
+            var d = new Day6().Part2();
             Console.WriteLine(d);
         }
     }
